86bd58936767741d6ee55855464a184707275235
Ycc <ycc2001@gmail.com>
2025-07-18 10:32:31 +0800

    Spring boot initialization

---
+++ src/main/java/com/ycc/likelist/LikelistApplication.java
@@ -0,0 +1,13 @@
+package com.ycc.likelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ycc/likelist/LikelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycc.likelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
